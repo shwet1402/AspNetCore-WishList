4baebe93c3c6cfb28db07c81e987020379be72da
Shwet Nisha <shwet.nisha32@gmail.com>
2021-09-17 12:46:58 -0700

    Completed module

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WishList.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController:Controller
     {
         public IActionResult Index()
         {
